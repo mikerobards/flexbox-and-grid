8d4a73a15417244208ad1c3a33c80a076411d94c
Mike Robards <mikerobards@icloud.com>
2021-05-25 16:51:41 -0400

    finish pokedex

--- pokedex/index.js
+++ pokedex/index.js
@@ -11,10 +11,22 @@
         Stats: HP/Attack/Defense/Speed
 */
 
+/*
+    Pokedex (Challenge)
+    
+    Write and utilize a 
+        displayPokedex(allPokemon) function 
+    
+    Display the Japanese, Chinese, and French
+        name of each Pokemon under its Stats
+        each should take up 1/3rd of the row 
+*/
+
+
 async function getAllPokemon() {
     let res = await fetch('pokemon.json')
     let data = await res.json()
-    return data
+    return data.slice(0,100)
 }
 
 function getPokemonHTML(aPokemon) {
@@ -27,12 +39,18 @@ function getPokemonHTML(aPokemon) {
         <div class="a-pokemon-stat">${aPokemon.base.Attack}</div>
         <div class="a-pokemon-stat">${aPokemon.base.Defense}</div>
         <div class="a-pokemon-stat">${aPokemon.base.Speed}</div>
+        <div class="a-pokemon-lang">${aPokemon.name.japanese}</div>
+        <div class="a-pokemon-lang">${aPokemon.name.chinese}</div>
+        <div class="a-pokemon-lang">${aPokemon.name.french}</div>
+
     </div>
 `
 }
 
-getAllPokemon().then(allPokemon => {
-    let samplePokemon = allPokemon[350]
-    document.body.innerHTML = `<div class="my-pokedex">${allPokemon.map(aPokemon => getPokemonHTML(aPokemon)).join('')}
-    </div>`
-})
+getAllPokemon().then(displayPokedex)
+
+function displayPokedex(allPokemon) {
+        document.body.innerHTML = `<div class="my-pokedex">
+        ${allPokemon.map(aPokemon => getPokemonHTML(aPokemon)).join('')}
+        </div>`
+    }
