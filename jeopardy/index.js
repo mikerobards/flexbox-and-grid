d6438ae2d24fa8f8779f2674ac2744a2b875de1d
Mike Robards <mikerobards@icloud.com>
2021-05-25 16:13:37 -0400

    finish jeopardy lesson

--- jeopardy/index.js
+++ jeopardy/index.js
@@ -24,24 +24,30 @@
 */
 
 
-async function getCategories() {
-    let res = await fetch('https://jservice.io/api/categories?count=4&offset=4')
+async function getCategories(count, offset) {
+    let res = await fetch(`https://jservice.io/api/categories?count=${count}&offset=${offset}`)
     let data = await res.json()
     console.log(data)
     return data
 }
 
+function getClueHTML(clueValue) {
+    return `<div class="my-category-clue" style="grid-row-start: ${clueValue / 100 + 1}">$${clueValue}</div>`
+}
+
 function getCategoryHTML(category) {
     return `
         <div class="my-category-title">${category.title}</div>
-        <div class="my-category-clue" style="grid-row-start: 2">$100</div>
-        <div class="my-category-clue" style="grid-row-start: 3">$200</div>
-        <div class="my-category-clue" style="grid-row-start: 4">$300</div>
-        <div class="my-category-clue" style="grid-row-start: 5">$400</div>
+        ${getClueHTML(100)}
+        ${getClueHTML(200)}
+        ${getClueHTML(300)}
+        ${getClueHTML(400)}
     `
 }
 
-getCategories().then(categories => {
+
+
+getCategories(5, 30).then(categories => {
     document.body.innerHTML = `
         <div class="board">
             ${categories.map(getCategoryHTML).join('')}
